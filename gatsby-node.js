e85810d189d768e5d993d4083e63cef0701723eb
Riley-Brown <riley@riley.gg>
2019-03-08 23:51:39 -0500

    Added functionality to dynamically create pages

---
+++ gatsby-node.js
@@ -0,0 +1,9 @@
+const path = require("path")
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  createPage({
+    path: "somefakepage",
+    component: path.resolve("./src/components/postLayout.js"),
+  })
+}
