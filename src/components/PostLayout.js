f17a828d4c85478e4b5d63d9025ae10cb82e2dd1
Riley-Brown <riley@riley.gg>
2019-03-08 23:52:15 -0500

    Created post layout component for dynamically generated pages

---
+++ src/components/PostLayout.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+export default class PostLayout extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Post Layout</h1>
+      </div>
+    )
+  }
+}
