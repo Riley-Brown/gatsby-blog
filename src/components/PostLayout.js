3c48bd7a7762328c9f688bb3718173fc874a96f1
Riley-Brown <riley@riley.gg>
2019-03-10 19:42:02 -0400

    made graphql query dynamic

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -4,17 +4,19 @@ import { graphql } from "gatsby"
 
 export default class PostLayout extends Component {
   render() {
+    const { markdownRemark } = this.props.data
     return (
       <Layout>
-        <h1>Post Layout</h1>
+        <h1>{markdownRemark.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
       </Layout>
     )
   }
 }
 
 export const query = graphql`
-  query PostQuery {
-    markdownRemark(frontmatter: { slug: { eq: "/second-post" } }) {
+  query PostQuery($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
       html
       frontmatter {
         title
