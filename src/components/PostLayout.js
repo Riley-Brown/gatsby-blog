50a9075f2966f765a09353edefa3ec22785aae0d
Riley-Brown <riley@riley.gg>
2019-03-09 23:28:15 -0500

    replaced placeholder div with Layout component

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react"
+import Layout from "./layout"
 
 export default class PostLayout extends Component {
   render() {
     return (
-      <div>
+      <Layout>
         <h1>Post Layout</h1>
-      </div>
+      </Layout>
     )
   }
 }
