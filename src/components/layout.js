472ab165a2abda8e6e0877b3662aee3426e0280d
Riley-Brown <riley@riley.gg>
2019-03-13 22:56:46 -0400

    changed max width to line up with nav max width

--- src/components/layout.js
+++ src/components/layout.js
@@ -32,9 +32,7 @@ const Layout = ({ children, location }) => (
         <div
           style={{
             margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            maxWidth: 1140,
           }}
         >
           <main>{children}</main>
