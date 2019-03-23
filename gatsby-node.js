7b9095b2ca3b9fd6cd253a6853a07f9dd9ebf6b6
Riley-Brown <riley@riley.gg>
2019-03-23 15:18:15 -0400

    added function to convert frontmatter to static images

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,9 @@
 const path = require("path")
+const { fmImagesToRelative } = require("gatsby-remark-relative-images")
+
+exports.onCreateNode = ({ node }) => {
+  fmImagesToRelative(node)
+}
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
