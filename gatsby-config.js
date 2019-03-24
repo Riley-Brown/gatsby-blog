ea90d93aa3461cee3a312f18d483d5e0fb8e5d75
Riley-Brown <riley@riley.gg>
2019-03-24 01:01:11 -0400

    removed transformer remark for possible bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     "gatsby-plugin-offline",
