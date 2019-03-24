98e5aab37ce0e58cecc26b2ffb86aea36e96017d
Riley-Brown <riley@riley.gg>
2019-03-23 23:22:30 -0400

    added transformer-remark to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     "gatsby-plugin-offline",
