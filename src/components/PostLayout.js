d0932634917710d8633e336b04cebbc7b0d03939
Riley-Brown <riley@riley.gg>
2019-03-23 23:23:17 -0400

    replaced img with gatsby Img component for lazy loading

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -2,13 +2,14 @@ import React, { Component } from "react"
 import Layout from "./layout"
 import { graphql } from "gatsby"
 import "./postLayout.css"
-// import Img from "gatsby-image"
+import Img from "gatsby-image"
 
 export default class PostLayout extends Component {
   render() {
+    console.log(this.props)
     const { markdownRemark } = this.props.data
     const { location } = this.props
-    console.log(this.props.data)
+
     return (
       <Layout location={location}>
         <div className="single-post">
@@ -17,8 +18,11 @@ export default class PostLayout extends Component {
             <span>{markdownRemark.frontmatter.author}</span>
             <span>{markdownRemark.frontmatter.date}</span>
           </div>
-          <img src={markdownRemark.frontmatter.cover_img} alt="" />
-          {/* <Img fluid={this.props.data.file.childImageSharp.fluid} /> */}
+          {/* <img
+            src={markdownRemark.frontmatter.cover_img}
+            alt="featured image"
+          /> */}
+          <Img fluid={this.props.data.file.childImageSharp.fluid} />
 
           <div dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
         </div>
@@ -28,8 +32,8 @@ export default class PostLayout extends Component {
 }
 
 export const query = graphql`
-  query PostQuery($slug: String!) {
-    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+  query PostQuery($cover_img: String!) {
+    markdownRemark(frontmatter: { cover_img: { eq: $cover_img } }) {
       html
       frontmatter {
         title
@@ -39,12 +43,12 @@ export const query = graphql`
         author
       }
     }
-    # file(relativePath: { regex: "/fullscreen/" }) {
-    #   childImageSharp {
-    #     fluid(maxWidth: 1000) {
-    #       ...GatsbyImageSharpFluid
-    #     }
-    #   }
-    # }
+    file(relativePath: { eq: $cover_img }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
   }
 `
