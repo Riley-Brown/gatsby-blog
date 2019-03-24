aad54ba7703a6c8e08d8e6487b016d1b83fc47d6
Riley-Brown <riley@riley.gg>
2019-03-24 16:26:16 -0400

    Added gatsby-transformer-remark and options for remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     "gatsby-plugin-offline",
@@ -46,6 +47,11 @@ module.exports = {
           },
           {
             resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1200,
+              quality: 75,
+              linkImagesToOriginal: false,
+            },
           },
           {
             resolve: "gatsby-remark-copy-linked-files",
